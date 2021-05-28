ad3ab33d04d5e2506011cc974f32ac356d2c76b1
calm_dh <opmina72@naver.com>
2021-05-28 12:59:50 +0900

    문제 풀이 코드

--- 8958.cpp
+++ 8958.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
-#include <iostream>
-#include <algorithm>
-using namespace std;
 int main(){
-
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        int score = 0;
+        int add = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'X'){
+                add = 0;
+            }else{
+                add++;
+            }
+            score += add;
+        }
+        cout << score << '\n';  
+    }
 }
